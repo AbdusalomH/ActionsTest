8e9a89dd683d06e2f3a3ff2a70e13d88702c4ce4
AbdusalomH <akhodjiev@gmail.com>
2025-01-04 11:50:32 +0500

    no message

---
+++ ActionsTests/MyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ActionsTests
+//
+//  Created by Abdusalom on 04/01/2025.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
