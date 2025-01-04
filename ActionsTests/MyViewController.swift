c6e750c33c41464c1593629f2e5768149f95f296
AbdusalomH <akhodjiev@gmail.com>
2025-01-04 13:27:14 +0500

    added new background

--- ActionsTests/MyViewController.swift
+++ ActionsTests/MyViewController.swift
@@ -13,7 +13,7 @@ class MyViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .yellow
+        view.backgroundColor = .green
     }
 
 
